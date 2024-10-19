fb06739e75eb995191f8ce06bef38d4334332e1e
Ashish Bogati <60840304+ashizZz@users.noreply.github.com>
2024-10-19 21:04:08 +0545

    Update url-rewrite.js

--- url-rewrite.js
+++ url-rewrite.js
@@ -3,5 +3,5 @@
 // Check if the URL contains 'StayUpdated.html'
 if (window.location.pathname === '/StayUpdated.html') {
     // Replace the URL in the browser address bar without reloading the page
-    window.history.replaceState(null, null, '/StayUpdated');
+    window.history.replaceState({}, '', '/stayupdated');
 }
