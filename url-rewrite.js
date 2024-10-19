59617274dd5faf60a6fc2de84f9debab8bc76138
Ashish Bogati <60840304+ashizZz@users.noreply.github.com>
2024-10-19 21:09:43 +0545

    Delete url-rewrite.js

--- url-rewrite.js
+++
@@ -1,9 +0,0 @@
-// url-alternate.js
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Check if the current URL contains 'StayUpdated.html'
-    if (window.location.pathname.endsWith('/StayUpdated.html')) {
-        // Update the URL in the address bar without reloading the page
-        window.history.pushState({}, '', '/stayupdated');
-    }
-});
